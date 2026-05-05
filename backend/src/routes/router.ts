e4907b88970f29e610e31b3c4e03c0957ffae799
MedNourS <me@nour-sabir.dev>
2026-05-05 15:41:47 -0400

    Protected userRouter with JWT auth middleware

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,10 +1,15 @@
 // Import Router and all routes
 import { Router } from 'express';
 import userRouter from './user';
+import authRouter from './auth';
 
 const router = Router(); // create the main router
 
+// Import authentication middleware for protected routes
+import { authenticateWithJWTCookie } from '../middleware/auth';
+
 // Use all routes
-router.use('/users', userRouter);
+router.use('/users', authenticateWithJWTCookie, userRouter);
+router.use('/auth', authRouter);
 
 export default router;
