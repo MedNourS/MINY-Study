3e4d6d099802a00ccd9e15f0d06175ae81470e37
MedNourS <me@nour-sabir.dev>
2026-05-05 15:40:23 -0400

    Added the cookieParser middleware to ensure cookie parsing of the request

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,14 @@
 // Import necessary modules for app functionality
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import logger from './middleware/logger';
 import router from './routes/router';
 
 // Make the app and add the required middleware and routes
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(logger);
 app.use(express.json());
 app.use('/api', router);
